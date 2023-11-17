2765af6d2510767bc12af41f9dc0477e618a4a8e
Carlos Adolfo Ortiz Quirós <carlosaortiz@yahoo.com>
2023-11-16 21:17:29 -0500

    Add initial files.

---
+++ src/test/java/com/polarbookshop/dispatcherservice/ApplicationTests.java
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------*/
+/* Source File:   APPLICATIONTESTS.JAVA                                       */
+/* Copyright (c), 2023 CSoftZ                                                 */
+/*----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------
+ History
+ Nov.16/2023  COQ  File created.
+ -----------------------------------------------------------------------------*/
+package com.polarbookshop.dispatcherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Unit tests for Application class.
+ *
+ * @author COQ - Carlos Adolfo Ortiz Q.
+ */
+@SpringBootTest
+class ApplicationTests {
+
+    /**
+     * Load Spring Boot contexts to validate it can run the application.
+     */
+    @Test
+    void contextLoads() {
+    }
+
+}
